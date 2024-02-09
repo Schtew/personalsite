da20630f09f4ee4547d6c42e3d30dfb60e93d062
amelone3 <amelone3@gatech.edu>
2024-02-09 17:46:37 +0000

    Adding support for hackathon.js

--- src/Components/Hackathon.js
+++ src/Components/Hackathon.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import { Card, CardContent, Typography, Grid, CardMedia } from '@material-ui/core';
 import Test from '../test';
+import basinlake from '../Assets/basinlakes.jpg';
+import OctoDashthumbnail from '../Assets/OctoDashthumbnail.jpg';
 
 const HackathonsPage = () => {
     const hackathons = [
         {
-            title: 'Hackathon 1',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            title: 'APEC & Google Asia 2020 App Challenge | 3rd Place',
+            image: OctoDashthumbnail,
+            link: 'https://devpost.com/software/octodash',
+            description:<ul>
+                <li>Became the first ever US team to podium in the international APEC 2020 App Challenge by building the smart inventory management web application Octodash.</li>
+                <li>Octodash uses machine learning to predict inventory levels against COVID cases to reduce waste, and was built using React.js, Node.js, and Google Cloud Platform.</li>
+                </ul>,
             date: 'October 10, 2022',
         },
         {
@@ -18,27 +25,41 @@ const HackathonsPage = () => {
     ];
 
     return (
-        <><Test />
-        <Grid container spacing={3}>
-            {hackathons.map((hackathon, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                    <Card>
-                        <CardContent>
-                            <Typography variant="h5" component="h2">
-                                {hackathon.title}
-                            </Typography>
-                            <Typography color="textSecondary" gutterBottom>
-                                {hackathon.date}
-                            </Typography>
-                            <Typography variant="body2" component="p">
-                                {hackathon.description}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            ))}
-        </Grid>
-        </>
+        <div style={{backgroundImage: `url(${basinlake})`, backgroundSize: 'cover', width: '100%', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 0, padding: 0}}>
+            <Grid container style={{width: '80%'}}>
+                {hackathons.map((hackathon, index) => (
+                    <Grid item xs={12} key={index} style={{margin: '1rem'}}>
+                        <Card>
+                            <Grid container>
+                                <Grid item xs={3} style={{alignItems: 'center', justifyContent: 'center'}}>
+                                    <a href={hackathon.link} style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '70%', margin:'1rem'}}>
+                                        <CardMedia
+                                            image={hackathon.image}
+                                            title={hackathon.title}
+                                            style={{width: '100%', height: '100%'}}
+                                        />
+                                    </a>
+                                </Grid>
+                                <Grid item xs = {1}></Grid>
+                                <Grid item xs={8}>
+                                    <CardContent>
+                                        <Typography variant="h6" component="h2">
+                                            {hackathon.title}
+                                        </Typography>
+                                        <Typography color="textSecondary" gutterBottom>
+                                            {hackathon.date}
+                                        </Typography>
+                                        <Typography variant="body2" component="p">
+                                            {hackathon.description}
+                                        </Typography>
+                                    </CardContent>
+                                </Grid>
+                            </Grid>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
     );
 };
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import '../fiordlands.jpg';
 import Test from '../test';
-import fiordlandsImage from '../fiordlands.jpg';
-import headshot from '../headshot.jpg';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import fiordlandsImage from '../Assets/fiordlands.jpg';
+import headshot from '../Assets/headshot.jpg';
+import { Card, CardContent, Typography, LinearProgress } from '@material-ui/core';
+import resume from '../Assets/resume.png';
 
 const Home = () => {
     return (
         <div style={{backgroundImage: `url(${fiordlandsImage})`, backgroundSize: 'cover', width: '100%', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
             <Card style={{width:'80%' }}>
                 <CardContent style={{display: 'flex', alignItems: 'center'}}>
-                    <div style={{width: '30%', height: '30%', borderRadius: '50%', overflow: 'hidden', marginRight: '3rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <img src={headshot} alt="Profile" style={{maxWidth: '100%', maxHeight: '100%', objectFit: 'cover'}} />
+                    <div style={{width: '30%', height: '30%', borderRadius: '50%', overflow: 'hidden', marginRight: '1rem', marginLeft: '1rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        <img src={headshot} alt="Profile" style={{maxWidth: '100%', maxHeight: '100%', minWidth: '50%', minHeight: '50%', objectFit: 'cover'}} />
                     </div>
                     <div>
                         <Typography variant="h5" component="h2" style={{"font-family": "Lora"}}>
@@ -24,24 +24,72 @@ const Home = () => {
                 </CardContent>
             </Card>
             <div style={{display: 'flex', flexDirection: 'row', marginTop: '2rem', width: '80%'}}>
-                <Card style={{marginRight: '1rem', flex: 1}}>
+                <Card style={{marginRight: '1rem', width: '20%'}}>
                     <CardContent>
-                        <Typography variant="h6" component="h3" style={{"font-family": "Lora"}}>
-                            Resume
+                        <Typography variant="h5" component="h3" style={{"font-family": "Lora", textAlign: 'center'}}>
+                            Resume 
                         </Typography>
                         <Typography color="textSecondary">
-                            Add your resume content here.
+                            <a href='https://gtvault-my.sharepoint.com/:b:/g/personal/amelone3_gatech_edu/EcbEdiNaQSFLqiBfSKHXSJgBiplYiAXWVHU-UvaMW-WmKA?e=IenWE1'>
+                                <img src={resume} alt="resume" style={{maxWidth: '100%', maxHeight: '100%', objectFit: 'cover'}} />
+                            </a>
                         </Typography>
                     </CardContent>
                 </Card>
-                <Card style={{flex: 1}}>
+                <Card style={{ flex: 1, width: '70%' }}>
                     <CardContent>
-                        <Typography variant="h6" component="h3" style={{"font-family": "Lora"}}>
+                        <Typography variant="h5" component="h3" style={{ fontFamily: "Lora", textAlign: 'center' }}>
                             Coding Abilities
                         </Typography>
-                        <Typography color="textSecondary">
-                            Add your skill meters here.
-                        </Typography>
+                        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                            <div style={{ width: '30%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+                                <Typography variant="h6" component="h3" style={{ fontFamily: "Lora", textAlign: 'center' }}>
+                                    Web Dev
+                                </Typography>
+                                <div style={{ flex: 1 }}>
+                                    <Typography color="textSecondary">JavaScript</Typography>
+                                    <LinearProgress variant="determinate" value={90} />
+                                </div>
+                                <div style={{ flex: 1 }}>
+                                    <Typography color="textSecondary">React.js</Typography>
+                                    <LinearProgress variant="determinate" value={80} />
+                                </div>
+                                <div style={{ flex: 1 }}>
+                                    <Typography color="textSecondary">Node.js</Typography>
+                                    <LinearProgress variant="determinate" value={80} />
+                                </div>
+                                <div style={{ flex: 1 }}>
+                                    <Typography color="textSecondary">TypeScript</Typography>
+                                    <LinearProgress variant="determinate" value={70} />
+                                </div>
+                            </div>
+                            <div style={{ width: '30%' }}>
+                                <Typography variant="h6" component="h3" style={{ fontFamily: "Lora", textAlign: 'center' }}>
+                                    Data
+                                </Typography>
+                                <Typography color="textSecondary">Python</Typography>
+                                <LinearProgress variant="determinate" value={90} />
+                                <Typography color="textSecondary">NumPy</Typography>
+                                <LinearProgress variant="determinate" value={80} />
+                                <Typography color="textSecondary">TensorFlow</Typography>
+                                <LinearProgress variant="determinate" value={80} />
+                                <Typography color="textSecondary">Pandas</Typography>
+                                <LinearProgress variant="determinate" value={70} />
+                                <Typography color="textSecondary">NoSQL</Typography>
+                                <LinearProgress variant="determinate" value={70} />
+                            </div>
+                            <div style={{ width: '30%' }}>
+                                <Typography variant="h6" component="h3" style={{ fontFamily: "Lora", textAlign: 'center' }}>
+                                    Tooling
+                                </Typography>
+                                <Typography color="textSecondary">Git</Typography>
+                                <LinearProgress variant="determinate" value={90} />
+                                <Typography color="textSecondary">AWS</Typography>
+                                <LinearProgress variant="determinate" value={70} />
+                                <Typography color="textSecondary">Google Cloud</Typography>
+                                <LinearProgress variant="determinate" value={60} />
+                            </div>
+                        </div>
                     </CardContent>
                 </Card>
             </div>
